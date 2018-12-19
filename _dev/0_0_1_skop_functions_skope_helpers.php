b1e3ca57485e734cfae8377e21199d6457cae8fe
nikeo <nicolas@presscustomizr.com>
2018-12-19 11:58:46 +0100

    fixed : don't print the skope error log if not in nimble dev mode. fixes #1

--- _dev/0_0_1_skop_functions_skope_helpers.php
+++ _dev/0_0_1_skop_functions_skope_helpers.php
@@ -148,7 +148,12 @@ function skp_get_skope( $_requesting_wot = null, $_return_string = true, $reques
                 $_return = array( "id" => "{$obj_id}" );
             }
             else {
-                error_log( __FUNCTION__ . ' error when building the local skope, no object_id provided.');
+                // don't print the skope error log if not in dev mode
+                // fixes : https://github.com/presscustomizr/czr-skope/issues/1
+                if ( defined( 'NIMBLE_DEV' ) && NIMBLE_DEV ) {
+                    error_log( __FUNCTION__ . ' error when building the local skope, no object_id provided.');
+                    error_log( print_r( $parts, true) );
+                }
             }
         break;
     }
